2375a0a914f665106f081e38a9f397d9096c8c2f
shiva <shivamoeini2000@gmail.com>
2022-04-20 02:29:14 +0430

    cmit8

--- express/quize2.js
+++ express/quize2.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+app.get("/newSearch", (req, res) => {
+  const { q, query } = req.query;
+  console.log(req.query);
+  res.send(`you searched for: ${q} with query of: ${query}`);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`your app is runing on port:${PORT}`);
